f438ccb782efa919c7c462c9dad2699445622129
Telugu Divya <tdivya.bsc23@rvu.edu.in>
2024-02-15 21:34:08 +0530

    test

--- set02/problem03.c
+++ set02/problem03.c
@@ -7,19 +7,16 @@ void output(int n, int result);
 int main() {
     int number, result;
 
-    // Input number
+    
     number = input_number();
 
-    // Check if the number is composite
     result = is_composite(number);
 
-    // Output result
     output(number, result);
 
     return 0;
 }
 
-// Function to input number
 int input_number() {
     int num;
     printf("Enter a number: ");
@@ -27,17 +24,15 @@ int input_number() {
     return num;
 }
 
-// Function to check if a number is composite
 int is_composite(int n) {
     int i, factors = 0;
     for (i = 1; i <= n; ++i) {
         if (n % i == 0)
             factors++;
     }
-    return (factors > 2); // If number of factors is greater than 2, it's composite
+    return (factors > 2); 
 }
 
-// Function to output result
 void output(int n, int result) {
     if (result)
         printf("%d is a composite number\n", n);
