7f2cc36ac9356b022ce24f62a2e88516d5f4e5df
Telugu Divya <tdivya.bsc23@rvu.edu.in>
2024-02-17 20:25:36 +0530

    test

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,7 +1,6 @@
  //Write a C program to compare three numbers using **pass by reference**
  #include<stdio.h>
 
-// Function declarations
 int input(int *a, int *b, int *c);
 void compare(int a, int b, int c, int *largest);
 void output(int a, int b, int c, int largest);
