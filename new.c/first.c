0c135b9054dc5a8ada37ebbd78364f6c8818f015
Telugu Divya <tdivya.bsc23@rvu.edu.in>
2024-02-20 20:54:50 +0530

    test

---
+++ new.c/first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int N = 3;
+
+void showboard(int board[N][N])
+{
+    int i , j;
+    for(i=o; i < N; i++)
+    {
+        for(j = 0; j < N; j++)
+        {
+            printf("%d", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    
+}
